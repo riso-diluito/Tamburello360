d4acb6de5d4bf8a914afdd024d4396157e16b854
riso-diluito <106911092+riso-diluito@users.noreply.github.com>
2026-02-10 16:46:19 +0100

    Update passthrough copy configuration in Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("public");
+  eleventyConfig.addPassthroughCopy({"public": "."});
   eleventyConfig.addPassthroughCopy("admin/**/*");
   
   eleventyConfig.addFilter("readableDate", dateObj => {
