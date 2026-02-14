efc44d4933888aea9823bd43185099ef2bc8b03b
riso-diluito <106911092+riso-diluito@users.noreply.github.com>
2026-02-14 16:59:48 +0100

    Change passthrough copy from 'public/admin' to 'public'

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("public/admin");
+  eleventyConfig.addPassthroughCopy("public");
   
   eleventyConfig.addFilter("readableDate", dateObj => {
     return new Date(dateObj).toLocaleDateString('it-IT', {
